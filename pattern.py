c474deb14b4827737b0eb9e1c97aff5a4c3d54e7
Raphael Kargon <raphael.kargon@gmail.com>
2015-09-10 23:14:31 -0400

    more work on pattern

--- pattern.py
+++ pattern.py
@@ -1,7 +1,15 @@
+class Motif():
+    def __init__(self, color, width, height, repeat_x, repeat_y, space_x, space_y):
+        self.color = color;
+        self.width = width;
+        self.height = height;
+        pass
+
 class Pattern():
 
-    def __init__(self):
-
+    def __init__(self, bgcol, motifs):
+        self.bgcol = bgcol
+        self.motifs = motifs
 
     @staticmethod
     def random_pattern():
