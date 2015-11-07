9a11e8d0a9eb056017d9342d26e474c23e564788
hollycontin@gmail.com <noah_picard@brown.edu>
2015-11-06 21:32:32 -0500

    Added contextualized colors (main, sub1, text, etc..), spread to full width, and added header

--- fashionserver.py
+++ fashionserver.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 import getcolor as gc
 from treetolayout import tree_to_layout, gen_tree
+import random
 
 app = Flask(__name__)
 
@@ -17,6 +18,11 @@ def get_colors():
     colors = gc.get_colors();
     return jsonify({'colors': colors}), 200
 
+@app.route('/get-layout-colors/', methods=['GET'])
+def get_layout_colors():
+    colors = gc.get_layout_details();
+    return jsonify({'colors': colors}), 200
+
 @app.route('/page-gen/')
 def page_gen():
     return app.send_static_file('page-generator.html')
@@ -24,20 +30,32 @@ def page_gen():
 
 @app.route('/gen-layout/', methods=['GET'])
 def gen_layout():
-    element_list = [{'weight': 2, 'data': 'Hello world!'},
-                    {'weight': 4, 'data': 'I am the master commander!'},
-                    {'weight': 1, 'data': 'Bruh'},
-                    {'weight': 2, 'data': 'Jenkins, you\'re a jerk'},
-                    {'weight': 1, 'data': 'Beep boop'},
-                    {'weight': 1, 'data': 'Making up words'},
-                    {'weight': 1, 'data': 'Fa la la la la'},
-                    {'weight': 1, 'data': 'gcd is probably 1 I mean lets be real'},
-                    {'weight': 1, 'data': '3.1415926535897932384626433...'},
-                    {'weight': 1, 'data': 'I am a blob'},
-                    {'weight': 1, 'data': 'What is space?'},
-                    {'weight': 1, 'data': 'Who you?'},
-                    {'weight': 1, 'data': 'new phone who dis'},
+    '''element_list = [{'weight': 2, 'data': '<p>Hello world!</p>'},
+                    {'weight': 4, 'data': '<p>I am the master commander!</p>'},
+                    {'weight': 1, 'data': '<p>Bruh</p>'},
+                    {'weight': 2, 'data': '<p>Jenkins, you\'re a jerk</p>'},
+                    {'weight': 1, 'data': '<p>Beep boop</p>'},
+                    {'weight': 1, 'data': '<p>Making up words</p>'},
+                    {'weight': 1, 'data': '<p>Fa la la la la</p>'},
+                    {'weight': 1, 'data': '<p>gcd is probably 1 I mean lets be real</p>'},
+                    {'weight': 1, 'data': '<p>3.1415926535897932384626433...</p>'},
+                    {'weight': 1, 'data': '<p>I am a blob</p>'},
+                    {'weight': 1, 'data': '<p>What is space?</p>'},
+                    {'weight': 1, 'data': '<p>Who you?</p>'},
+                    {'weight': 1, 'data': '<p>new phone who dis</p>'},
+                    {'weight': 1, 'type': 'image', 'data': '<img src=""\>'},
+                    ]'''
+
+    element_list = [{'weight': 2, 'data': '<p>Hello world!</p>'},
+                    {'weight': 4, 'data': '<p>I am the master commander!</p>'},
+                    {'weight': 1, 'data': '<p>Bruh</p>'},
+                    {'weight': 2, 'data': '<p>Jenkins, you\'re a jerk</p>'},
+                    {'weight': 1, 'data': '<p>Beep boop</p>'},
+                    {'weight': 1, 'data': '<p>Making up words</p>'},
+                    {'weight': 1, 'type': 'image', 'data': '<img src="/static/images/SalazarGames.png"\>'},
+                    {'weight': 1, 'type': 'image', 'data': '<img src="/static/images/swingsprofpic.jpg"\>'},
                     ]
+    random.shuffle(element_list)
     return tree_to_layout(gen_tree(element_list, 0) )
 
 if __name__ == '__main__':
--- getcolor.py
+++ getcolor.py
@@ -1,42 +1,62 @@
-import random
+from random import *
 import math
 import colorsys
-# import numpy as np
 from colutils import get_random_color, make_color_hsl
 
 pattern_list = {
-    'complementary': [[0, 0, 0], [180, 0, 0]],
-    'complementary /w black': [[0, 0, 0], ['STATIC', 0, 0, 10], [180, 0, 0]],
-    'complementary /w white': [[0, 0, 0], ['STATIC', 0, 0, 99], [180, 0, 0]],
-    'split complementary': [[0, 0, 0], [150, 0, 0], [210, 0, 0]],
-    'analog': [[330, 0, 0], [0, 0, 0], [30, 0, 0]],
-    'double split': [[330, 0, 0], [30, 0, 0], [150, 0, 0], [210, 0, 0]],
-    'triadic': [[0, 0, 0], [120, 0, 0], [240, 0, 0]],
-    'monochrome': [[0, 0, 0], [0, 0, 25], [0, 0, 50], [0, 0, 75]]
+    'complementary': {
+        'colors':[[0, 0, 0], [180, 0, 0]]},
+    'complementary /w black': {
+        'colors':[[0, 0, 0], ['STATIC', 0, 0, 10], [180, 0, 0]]},
+    'complementary /w white': {
+        'colors':[[0, 0, 0], ['STATIC', 0, 0, 99], [180, 0, 0]]},
+    'split complementary': {
+        'colors':[[0, 0, 0], [150, 0, 0], [210, 0, 0]]},
+    'analog': {
+        'colors':[[330, 0, 0], [0, 0, 0], [30, 0, 0]]},
+    'double split': {
+        'colors':[[330, 0, 0], [30, 0, 0], [150, 0, 0], [210, 0, 0]]},
+    'triadic': {
+        'colors':[[0, 0, 0], [120, 0, 0], [240, 0, 0]]},
+    'monochrome': {
+        'colors':[[0, 0, 0], [0, 0, 25], [0, 0, 50], [0, 0, 75]]}
 }
 
+hue_max = 360
+sat_max = 100
+light_max = 100
 
-def get_colors():
-    hue_max = 360
-    sat_max = 100
-    light_max = 100
 
-    sat_ceil = 90
-    light_ceil = 80
+def get_random_color(hue_floor=0, hue_ceil=360, sat_floor=0, sat_ceil=100, light_floor=0, light_ceil=100):
+    '''
+    Generates a random color using hsl values within the given ranges
+    '''
+    hue = (random() * (hue_ceil - hue_floor)) + hue_floor
+    sat = (random() * (sat_ceil - sat_floor)) + sat_floor
+    light = (random() * (light_ceil - light_floor)) + light_floor
+    return hue, sat, light
+
+
+def get_color_list():
+    '''
+    Generates a random color set
+    '''
+    sat_ceil = 100
+    light_ceil = 85
 
     sat_floor = 20
     light_floor = 20
 
-    sat = (random.random() * (sat_ceil - sat_floor)) + sat_floor
-    light = (random.random() * (light_ceil - light_floor)) + light_floor
+    hue, sat, light = get_random_color(sat_ceil=sat_ceil,
+                                       sat_floor=sat_floor,
+                                       light_floor=light_floor,
+                                       light_ceil=light_ceil)
+
+    ind = int(random() * len(pattern_list))
+    pattern_name = pattern_list.keys()[ind]
+    pattern = pattern_list[pattern_name]['colors']
 
     color_list = []
-
-    hue = (random.random() * hue_max)
-
-    ind = int(random.random() * len(pattern_list))
-    pattern_name = pattern_list.keys()[ind]
-    pattern = pattern_list[pattern_name]
 
     for p in pattern:
         if p[0] == 'STATIC':
@@ -48,8 +68,26 @@ def get_colors():
             color_sat = (sat + p[1]) % sat_max
             color_light = (light + p[2]) % light_max
 
-        hsl_list = [color_hue, color_sat, color_light]
-        rgb_list = colorsys.hls_to_rgb(hsl_list[0] / hue_max, hsl_list[2] / light_max, hsl_list[1] / sat_max);
+        color_hue   = round(color_hue, 2)
+        color_sat   = round(color_sat, 2)
+        color_light = round(color_light, 2)
+
+        color_list.append([color_hue, color_sat, color_light])
+
+    return color_list
+
+
+def get_colors():
+    color_list = get_color_list()
+    detail_list = []
+    for hsl_list in color_list:
+        color_hue = hsl_list[0]
+        color_sat = hsl_list[1]
+        color_light = hsl_list[2]
+        
+        rgb_list = colorsys.hls_to_rgb(hsl_list[0] / hue_max,
+                                       hsl_list[2] / light_max,
+                                       hsl_list[1] / sat_max);
         rgb_max = 255
         rgb_list = map(lambda x: int(round(x * rgb_max)), rgb_list)
         hsl_list = [round(hsl_list[0]) % hue_max, round(hsl_list[1]), round(hsl_list[2])]
@@ -57,15 +95,109 @@ def get_colors():
         rgb_str2 = 'rgb(' + str(rgb_list[0]) + ',' + str(rgb_list[1]) + ',' + str(rgb_list[2]) + ');'
         print rgb_str2
 
-        print hsl_list
-        print rgb_list
 
-        color_list.append([make_color_hsl(color_hue, color_sat, color_light),
+        detail_list.append([make_color_hsl(color_hue, color_sat, color_light),
                            make_color_hsl(round(color_hue), round(color_sat), round(color_light)) +
-                           ' | ' + rgb_str2 + ' | ' + rgb_str])
+                           ' | ' + rgb_str2 + ' | ' + rgb_str, hsl_list])
+    return detail_list
 
-    print color_list
+def get_color_detail(color):
+    hsl_list = color
+    color_hue = hsl_list[0]
+    color_sat = hsl_list[1]
+    color_light = hsl_list[2]
+        
+    rgb_list = colorsys.hls_to_rgb(hsl_list[0] / hue_max, hsl_list[2] / light_max, hsl_list[1] / sat_max);
+    rgb_max = 255
+    rgb_list = map(lambda x: int(round(x * rgb_max)), rgb_list)
+    hsl_list = [round(hsl_list[0]) % hue_max, round(hsl_list[1]), round(hsl_list[2])]
+    rgb_str = '#' + reduce(lambda x, y: str(x) + format(y, '02x'), rgb_list, '')
+    rgb_str2 = 'rgb(' + str(rgb_list[0]) + ',' + str(rgb_list[1]) + ',' + str(rgb_list[2]) + ');'
+    print rgb_str2
 
-    random.shuffle(color_list)
 
-    return color_list
+    details = [make_color_hsl(color_hue, color_sat, color_light), make_color_hsl(round(color_hue), round(color_sat), round(color_light)) +
+                           ' | ' + rgb_str2 + ' | ' + rgb_str]
+    return details
+
+def get_layout_details():
+    col_obj = get_layout_colors()
+
+    for ct in col_obj:
+        if ct == "len":
+            continue
+        col = col_obj[ct]
+        col_obj[ct] = get_color_detail(col) + [col]
+    return col_obj
+
+def get_layout_colors():
+    color_list = get_color_list()
+    l = len(color_list)
+    col_obj = {}#{"main":None,"sub1":None, "sub2":None, "accent":None, "text":None, "len":0}
+    col_obj["len"] = l
+    #b_col, color_list = get_furthest_color(color_list, 1)
+
+    t_col = get_text_color(get_blend_color(color_list[1:3]))#get_contrast_color(f_col, 2, 50, 50)
+
+    
+    if (l == 2):
+        col_obj["main"] = color_list[0]
+        col_obj["sub1"] = color_list[1]
+        col_obj["text"] = t_col
+    elif (l == 3):
+        col_obj["main"] = color_list[0]
+        col_obj["sub2"] = color_list[2]
+        col_obj["sub1"] = color_list[1]
+        col_obj["text"] = t_col
+    elif (l == 4):
+        col_obj["main"] = color_list[0]
+        col_obj["sub2"] = color_list[2]
+        col_obj["accent"] = color_list[3]
+        col_obj["sub1"] = color_list[1]
+        col_obj["text"] = t_col
+    
+    return col_obj
+
+
+def get_contrast_color(col, att, mid, dist):
+    ncol = col[:]
+    if ncol[att] > mid:
+        dist *= -1
+    ncol[att] += dist
+    return ncol
+
+def get_blend_color(color_list):
+    l = float(len(color_list))
+    hue = sum(zip(*color_list)[0])/l
+    sat = sum(zip(*color_list)[1])/l
+    light = sum(zip(*color_list)[2])/l
+
+    col = [hue, sat, light]
+    return col
+
+
+def get_text_color(col):
+    ncol = [0,0,99]
+    if col[2] > 50:
+        ncol = [0,0,10]
+    return ncol
+    
+
+def get_furthest_color(color_list, att):
+    m_light = sum(zip(*color_list)[att])/len(color_list)
+
+    diff = 0
+    idx = -1 
+    for i in xrange(len(color_list)):
+        col = color_list[i]
+        d = m_light - col[att]
+        if abs(d) > diff:
+            diff = d
+            idx = i
+    f_col = color_list[idx]
+    color_list = color_list[:idx] + color_list[idx+1:]
+    return f_col, color_list
+
+
+
+
